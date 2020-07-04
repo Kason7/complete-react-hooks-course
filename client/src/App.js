97fa8b0800a55dc92564414dad44967d57b9a7b5
Kasper Sonne <kaspersonne27@gmail.com>
2020-07-04 11:30:00 +0200

    Section 2: Lesson 15 - useState Hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
 
 // Import components
@@ -7,6 +6,19 @@ import NameTag from './components/nameTag';
 import Input from './components/Input';
 
 function App() {
+  // Initial state
+  const [age, setAge] = useState(21);
+  // State methods
+  const ageUp = () => {
+    const newAge = age + 1;
+    setAge(newAge);
+  };
+  const ageDown = () => {
+    const newAge = age - 1;
+    setAge(newAge);
+  };
+
+  // Component body
   return (
     <div className='App'>
       <header className='App-header'>
@@ -16,6 +28,10 @@ function App() {
         <NameTag firstname='James' lastname='Joanson' />
         <NameTag />
         <Input placeholder='Enter here' type='text' />
+        <button onClick={ageDown}>-</button>
+        <h2>Age: {age}</h2>
+        <button onClick={ageUp}>+</button>
+        <hr />
       </header>
     </div>
   );
